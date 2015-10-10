a8b659edecc8f15b76d0f203110aa51b237913af
Steven 'Steve' Kendall <skendall@neverware.com>
2015-10-10 15:51:26 -0400

    added a setup.py and named the module pigtwist
    this is for distinction from other projects combining twisted and pyglet

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pigtwist',
+      version='1.0',
+      description='Pyglet-Twisted reactor',
+      author='Pádraig Kitterick',
+      url='https://github.com/padraigkitterick/pyglet-twisted/',
+      packages=['pigtwist'],
+     )
